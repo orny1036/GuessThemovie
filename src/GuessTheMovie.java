e8cc9ae7160e6f39ef69c48d1ec51d3e220cda58
nabila2022831036 <ornynabila@gmail.com>
2025-05-10 01:08:33 +0600

    Added BufferedReader FileReader Random number generator for more advance implementation

--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -1,10 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessTheMovie {
    public static void main(String[] args)
    {
-       String name = "notebook";
+       String filePath = "words.txt";
+       ArrayList<String>movies = new ArrayList<>();
+       try(BufferedReader reader = new BufferedReader(new FileReader(filePath)))
+       {
+           String line;
+           while ((line = reader.readLine())!=null)
+           {
+               movies.add(line.trim());
+           }
+       }
+    catch(FileNotFoundException e)
+    {
+        System.out.println("Could not find file.");
+    }
+       catch (IOException e)
+       {
+           System.out.println("Something went wrong.");
+       }
+       System.out.println(movies);
+       Random random = new Random();
+       String name = movies.get(random.nextInt(movies.size()));
+       System.out.println("The choosen movie is " + name);
+       // initial program
        Scanner scanner = new Scanner(System.in);
        ArrayList<Character>wordGame = new ArrayList<>();
        int wrongGuesses = 0, correctGuesses = 0;
@@ -48,7 +75,10 @@ public class GuessTheMovie {
               System.out.println("Ahh, wrong guess!");
               wrongGuesses++;
               System.out.println("You have made "+wrongGuesses+" wrong guesses.");
+              if(wrongGuesses<=6)
               System.out.println(getArt(wrongGuesses));
+              else
+                  System.out.println(getArt(6));
           }
 
        }
@@ -64,48 +94,47 @@ public class GuessTheMovie {
        //System.out.println(getArt(wrongGuesses));
        scanner.close();
    }
-   static String getArt(int wrongGuesses)
-   {
-      return switch (wrongGuesses)
-       {
+
+   static String getArt(int wrongGuesses) {
+       return switch (wrongGuesses) {
            case 0 -> """
-                     
-                     
-                     
-                     """;
+                   
+                   
+                   
+                   """;
            case 1 -> """
-                      o
-                    
-                     
-                     """;
-           case 2-> """
-                     o
-                     | 
-                     
-                    """;
-           case 3-> """
-                     o
-                    /| 
-                     
-                    """;
-           case 4->"""
+                    o
+                   
+                   
+                   """;
+           case 2 -> """
+                    o
+                    | 
+                   
+                   """;
+           case 3 -> """
+                    o
+                   /| 
+                   
+                   """;
+           case 4 -> """
                      o
                     /|\\ 
-                     
+                   
                    """;
-           case 5->"""
+           case 5 -> """
                      o
                     /|\\ 
                     /
                    """;
-           case 6->"""
+           case 6 -> """
                      o
                     /|\\ 
                     / \\
                    """;
            default -> """
-                      something went wrong :(
-                      """;
+                   something went wrong :(
+                   """;
        };
    }
 }
